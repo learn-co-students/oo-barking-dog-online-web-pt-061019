67b4701757823f1cdc4ff2f9a81d81e6ffb412c3
Christian Polanco <chrispolanco@sbcglobal.net>
2019-06-27 04:44:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
-def bark
-  puts "Bark"
-end
+class Dog
+  def start  
+    bark
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+  def name=(dog_name)
+    @no_name = dog_name
+  end
+
+  def name 
+    @no_name
+  end
+
+end 
